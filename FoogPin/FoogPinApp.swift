ae187d36fafd35f0f1fe1ee47e321a6a31905397
Nutpupicha <6510615096@student.tu.ac.th>
2025-03-19 13:43:28 +0700

    Initial Commit

---
+++ FoogPin/FoogPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoogPinApp.swift
+//  FoogPin
+//
+//  Created by นางสาวณัฐภูพิชา อรุณกรพสุรักษ์ on 19/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct FoogPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
